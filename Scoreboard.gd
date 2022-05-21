9d4aadaa51ac4c0e9d6f13c4b31aff4369e59571
jonas <jonbo278@student.liu.se>
2022-05-21 17:37:02 +0200

    reformated scoreboard and should now only show on self

--- Scoreboard.gd
+++ Scoreboard.gd
@@ -8,7 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("scoreboard"):
+	if is_network_master() and Input.is_action_pressed("scoreboard"):
 		show()
 		var names = "Player\n"
 		var kills = "Kills\n"
