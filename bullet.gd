66812264e30bcbe79b917888373d33ab262eae8a
BuildTools <unconfigured@null.spigotmc.org>
2022-05-21 16:51:40 +0200

    calls killed

--- bullet.gd
+++ bullet.gd
@@ -33,8 +33,8 @@ func _on_Area2D_body_entered(body):
 	if player_id != parent_id:
 		# Colliding with a player
 		if body.has_method("hit_by_bullet"):
-			body.rpc("hit_by_bullet", parent_id)
-		if body.has_method("kiled"):
+			body.rpc("hit_by_bullet")
+		if body.has_method("killed"):
 			body.killed(parent_id)
 		# Colliding with the map
 		rpc("remove")
