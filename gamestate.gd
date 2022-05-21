19348823f68d3680f9a0889befb646a0990234e4
eliasjohansson <eljoh633@student.liu.se>
2022-05-21 10:21:19 +0200

    quick fix

--- gamestate.gd
+++ gamestate.gd
@@ -64,7 +64,7 @@ func setup_map(id):
 	
 	# Init player and add to map
 	player.set_name(str(id)) # Standard ID for server
-	player.get_node("Camera2D").Current = true
+	player.get_node("Camera2D").current = true
 	#player.position = spawn_pos
 	player.set_network_master(id)
 	map.get_node("Players").add_child(player)
