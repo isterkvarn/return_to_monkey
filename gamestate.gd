fa1ab6aec766f6c0bdcde481ea75788202439bf1
eliasjohansson <eljoh633@student.liu.se>
2022-05-21 14:56:21 +0200

    fixedname and color bug

--- gamestate.gd
+++ gamestate.gd
@@ -7,10 +7,15 @@ const MAX_PEERS = 12
 var peer = null
 
 var player_name = "monkee"
+var color_hue = 1.0
+
 
 var players = {}
 
 func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	color_hue = rng.randf_range(0, 1.0)
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("connected_to_server", self, "_connected_ok")
 	get_tree().connect("connection_failed", self, "_connected_fail")
@@ -31,7 +36,7 @@ func _player_connected(id):
 func _player_disconnected(id):
 	remove_player(id)
 	
-remote func add_player(id):
+remote func add_player(id, new_player_name, new_player_hue):
 	var map = get_tree().get_root().get_node("main").get_node("map")
 	var player = load("res://Player.tscn").instance()
 	#var spawn_pos = map.get_node("SpawnPoint").position
@@ -39,8 +44,9 @@ remote func add_player(id):
 	# Init player and add to map
 	player.set_name(str(id)) # Standard ID for server
 	player.set_network_master(id)
-	player.get_node("Label").set_text(player_name)
-	#player.position = spawn_pos
+	player.get_node("Label").set_text(new_player_name)
+	player.get_node("sprites").get_node("sprite_pants").modulate = Color.from_hsv(new_player_hue,0.8,0.9,1)
+	player.position = get_tree().get_root().get_node("main").get_node("map").get_node("SpawnPoints").get_random_spawn_position()
 	map.get_node("Players").add_child(player)
 
 remote func remove_player(id):
@@ -80,9 +86,7 @@ func setup_map(id):
 	player.set_name(str(id))
 	player.get_node("Camera2D").current = true
 	#player.position = spawn_pos
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	player.get_node("sprites").get_node("sprite_pants").modulate = Color.from_hsv(rng.randf_range(0, 1.0),0.8,0.9,1)
+	player.get_node("sprites").get_node("sprite_pants").modulate = Color.from_hsv(color_hue,0.8,0.9,1)
 	player.get_node("Label").set_text(player_name)
 	player.set_network_master(id)
 	map.get_node("Players").add_child(player)
