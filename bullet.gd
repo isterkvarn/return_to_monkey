e199df9294e8a961ff82846fd1948684e7bfb932
Gurka2 <martin.hogstedt@hotmail.com>
2022-05-21 10:51:47 +0200

    Bullet collides with walls and other players

--- bullet.gd
+++ bullet.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 const BULLET_SPEED = 100
 var speed = Vector2()
+var parent_id
+
+func set_parent_id(id):
+	parent_id = id
 
 func _ready():
 	speed.x = BULLET_SPEED
@@ -18,3 +22,15 @@ func shoot(origin, angle):
 	
 func _on_Area2D_area_entered(area):
 	queue_free()
+
+func _on_Area2D_body_entered(body):
+	# Collision detection
+	if body.has_method("hit_by_bullet"):
+		if body.has_method("get_player_id"):
+			var player_id = body.get_player_id()
+			if player_id != parent_id:
+				body.hit_by_bullet()
+				print("PLAYER")
+	elif body.name == "StaticBody2D":
+		print("STATIC")
+	#print("Collision")
