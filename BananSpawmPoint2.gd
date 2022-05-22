04458cb3965e00b2caea62b210dfdacd46c41604
Gurka2 <martin.hogstedt@hotmail.com>
2022-05-22 12:15:57 +0200

    added the new spawns to the map

--- BananSpawmPoint2.gd
+++ BananSpawmPoint2.gd
@@ -4,7 +4,6 @@ const RESPAWNTIME = 10
 
 var time = 0
 var isBanana = true
-var isActive = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +11,8 @@ func _ready():
 	pass
 
 func _process(delta):
+	var overlapping_bodies = get_overlapping_bodies()
+	
 	# Spawn a new banana
 	if not isBanana and time > RESPAWNTIME:
 		isBanana = true
@@ -20,15 +21,16 @@ func _process(delta):
 	# Decrease time to spawn a new banana
 	elif not isBanana and time < RESPAWNTIME:
 		time += delta
+	# There is banana so check for players to give it to
+	else:
+		if overlapping_bodies.size() != 0:
+			if overlapping_bodies[0].has_method("pick_up_banana"):
+				overlapping_bodies[0].pick_up_banana()
+				time = 0
+				isBanana = false
+				rpc("deactivate_banana")
 
-func _on_Node2D_body_entered(body):
-	if body.has_method("pick_up_banana") and isActive:
-		isActive = false
-		body.pick_up_banana()
-		rpc("deactivate_banana")
-		
-
-func activate_banana():
+remotesync func activate_banana():
 	$Sprite.show()
 
 remotesync func deactivate_banana():
